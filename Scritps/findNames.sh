ff4d714713b42df28338a97c3af7e8e42dab24ca
Maverick D. Ayos <lozziajuan@gmail.com>
2022-08-10 15:15:47 -0300

    Delete Scritps directory
    Nombre de la carpeta mal escrito

--- Scritps/findNames.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-# Se usa grep con la opcion de remarcar con color las palabras que empiezan con mayuscúlas.
-# Se muestra el texto con las palabras en mayusculas resaltadas.
-ARCHIVO=$1
-grep --color='always' "\<[A-Z][^ ]*\>" $ARCHIVO
